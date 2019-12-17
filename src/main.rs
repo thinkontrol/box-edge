ce8f3e593e440b16b9bd8388618e78a2b5863f0a
Shen Yang <shenyang.sx@outlook.com>
2019-12-17 21:57:39 +0800

    start extern

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ extern crate chrono;
 extern crate clap;
 extern crate env_logger;
 extern crate futures;
+extern crate libc;
 extern crate log;
 extern crate regex;
-extern crate libc;
 
 use chrono::Local;
 use clap::{App, Arg};
@@ -14,115 +14,192 @@ use futures::{Future, Stream};
 use log::{error, info, warn, LevelFilter};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 use std::io::Write;
 use std::time::Duration;
 use std::{env, process};
-use libc::size
 
-#[repr(C)] struct S7Object { _private: [u8; 0] }
+use libc::{c_int, c_void, Elf32_Word};
+
+struct S7Object {}
 
 #[link(name = "snap7")]
-extern {
-    fn Cli_Create() -> *S7Object;
-    fn Cli_Destroy(libc::by);
-int S7API Cli_ConnectTo(S7Object Client, const char *Address, int Rack, int Slot);
-int S7API Cli_SetConnectionParams(S7Object Client, const char *Address, word LocalTSAP, word RemoteTSAP);
-int S7API Cli_SetConnectionType(S7Object Client, word ConnectionType);
-int S7API Cli_Connect(S7Object Client);
-int S7API Cli_Disconnect(S7Object Client);
-int S7API Cli_GetParam(S7Object Client, int ParamNumber, void *pValue);
-int S7API Cli_SetParam(S7Object Client, int ParamNumber, void *pValue);
-int S7API Cli_SetAsCallback(S7Object Client, pfn_CliCompletion pCompletion, void *usrPtr);
-// Data I/O main functions
-int S7API Cli_ReadArea(S7Object Client, int Area, int DBNumber, int Start, int Amount, int WordLen, void *pUsrData);
-int S7API Cli_WriteArea(S7Object Client, int Area, int DBNumber, int Start, int Amount, int WordLen, void *pUsrData);
-int S7API Cli_ReadMultiVars(S7Object Client, PS7DataItem Item, int ItemsCount);
-int S7API Cli_WriteMultiVars(S7Object Client, PS7DataItem Item, int ItemsCount);
-// Data I/O Lean functions
-int S7API Cli_DBRead(S7Object Client, int DBNumber, int Start, int Size, void *pUsrData);
-int S7API Cli_DBWrite(S7Object Client, int DBNumber, int Start, int Size, void *pUsrData);
-int S7API Cli_MBRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_MBWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_EBRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_EBWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_ABRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_ABWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_TMRead(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_TMWrite(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_CTRead(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_CTWrite(S7Object Client, int Start, int Amount, void *pUsrData);
-// Directory functions
-int S7API Cli_ListBlocks(S7Object Client, TS7BlocksList *pUsrData);
-int S7API Cli_GetAgBlockInfo(S7Object Client, int BlockType, int BlockNum, TS7BlockInfo *pUsrData);
-int S7API Cli_GetPgBlockInfo(S7Object Client, void *pBlock, TS7BlockInfo *pUsrData, int Size);
-int S7API Cli_ListBlocksOfType(S7Object Client, int BlockType, TS7BlocksOfType *pUsrData, int *ItemsCount);
-// Blocks functions
-int S7API Cli_Upload(S7Object Client, int BlockType, int BlockNum, void *pUsrData, int *Size);
-int S7API Cli_FullUpload(S7Object Client, int BlockType, int BlockNum, void *pUsrData, int *Size);
-int S7API Cli_Download(S7Object Client, int BlockNum, void *pUsrData, int Size);
-int S7API Cli_Delete(S7Object Client, int BlockType, int BlockNum);
-int S7API Cli_DBGet(S7Object Client, int DBNumber, void *pUsrData, int *Size);
-int S7API Cli_DBFill(S7Object Client, int DBNumber, int FillChar);
-// Date/Time functions
-int S7API Cli_GetPlcDateTime(S7Object Client, tm *DateTime);
-int S7API Cli_SetPlcDateTime(S7Object Client, tm *DateTime);
-int S7API Cli_SetPlcSystemDateTime(S7Object Client);
-// System Info functions
-int S7API Cli_GetOrderCode(S7Object Client, TS7OrderCode *pUsrData);
-int S7API Cli_GetCpuInfo(S7Object Client, TS7CpuInfo *pUsrData);
-int S7API Cli_GetCpInfo(S7Object Client, TS7CpInfo *pUsrData);
-int S7API Cli_ReadSZL(S7Object Client, int ID, int Index, TS7SZL *pUsrData, int *Size);
-int S7API Cli_ReadSZLList(S7Object Client, TS7SZLList *pUsrData, int *ItemsCount);
-// Control functions
-int S7API Cli_PlcHotStart(S7Object Client);
-int S7API Cli_PlcColdStart(S7Object Client);
-int S7API Cli_PlcStop(S7Object Client);
-int S7API Cli_CopyRamToRom(S7Object Client, int Timeout);
-int S7API Cli_Compress(S7Object Client, int Timeout);
-int S7API Cli_GetPlcStatus(S7Object Client, int *Status);
-// Security functions
-int S7API Cli_GetProtection(S7Object Client, TS7Protection *pUsrData);
-int S7API Cli_SetSessionPassword(S7Object Client, char *Password);
-int S7API Cli_ClearSessionPassword(S7Object Client);
-// Low level
-int S7API Cli_IsoExchangeBuffer(S7Object Client, void *pUsrData, int *Size);
-// Misc
-int S7API Cli_GetExecTime(S7Object Client, int *Time);
-int S7API Cli_GetLastError(S7Object Client, int *LastError);
-int S7API Cli_GetPduLength(S7Object Client, int *Requested, int *Negotiated);
-int S7API Cli_ErrorText(int Error, char *Text, int TextLen);
-// 1.1.0
-int S7API Cli_GetConnected(S7Object Client, int *Connected);
+extern "C" {
+    fn Cli_Create() -> *mut S7Object;
+    fn Cli_Destroy(client: &S7Object);
+
+    fn Cli_ConnectTo(clinet: *mut S7Object, address: *const u8, rack: c_int, slot: c_int) -> c_int;
+    fn Cli_SetConnectionParams(
+        client: *mut S7Object,
+        address: *const u8,
+        localTSAP: Elf32_Word,
+        remoteTSAP: Elf32_Word,
+    ) -> c_int;
+    fn Cli_SetConnectionType(clinet: *mut S7Object, connectionType: Elf32_Word) -> c_int;
+    fn Cli_Connect(clinet: *mut S7Object) -> c_int;
+    fn Cli_Disconnect(clinet: *mut S7Object) -> c_int;
+    fn Cli_GetParam(clinet: *mut S7Object, paramNumber: c_int, pValue: *mut c_void) -> c_int;
+    fn Cli_SetParam(clinet: *mut S7Object, paramNumber: c_int, pValue: *mut c_void) -> c_int;
+    // fn Cli_SetAsCallback(clinet: *mut S7Object, pfn_CliCompletion pCompletion, void *usrPtr) -> c_int;
+    // Data I/O main functions
+    fn Cli_ReadArea(
+        clinet: *mut S7Object,
+        area: c_int,
+        dbNumber: c_int,
+        start: c_int,
+        amount: c_int,
+        wordLen: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_WriteArea(
+        clinet: *mut S7Object,
+        area: c_int,
+        dbNumber: c_int,
+        start: c_int,
+        amount: c_int,
+        wordLen: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    // fn Cli_ReadMultiVars(clinet: *mut S7Object, item: *mut TS7DataItem, itemsCount: c_int) -> c_int;
+    // fn Cli_WriteMultiVars(clinet: *mut S7Object, item: *mut TS7DataItem, itemsCount: c_int) -> c_int;
+    // Data I/O Lean functions
+    fn Cli_DBRead(
+        clinet: *mut S7Object,
+        dbNumber: c_int,
+        start: c_int,
+        size: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_DBWrite(
+        clinet: *mut S7Object,
+        dbNumber: c_int,
+        start: c_int,
+        size: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_MBRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void)
+        -> c_int;
+    fn Cli_MBWrite(
+        clinet: *mut S7Object,
+        start: c_int,
+        size: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_EBRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void)
+        -> c_int;
+    fn Cli_EBWrite(
+        clinet: *mut S7Object,
+        start: c_int,
+        size: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_ABRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void)
+        -> c_int;
+    fn Cli_ABWrite(
+        clinet: *mut S7Object,
+        start: c_int,
+        size: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_TMRead(
+        clinet: *mut S7Object,
+        start: c_int,
+        amount: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_TMWrite(
+        clinet: *mut S7Object,
+        start: c_int,
+        amount: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_CTRead(
+        clinet: *mut S7Object,
+        start: c_int,
+        amount: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    fn Cli_CTWrite(
+        clinet: *mut S7Object,
+        start: c_int,
+        amount: c_int,
+        pUsrData: *mut c_void,
+    ) -> c_int;
+    // Directory functions
+    // fn Cli_ListBlocks(clinet: *mut S7Object, pUsrData: *mut TS7BlocksList) -> c_int;
+    // fn Cli_GetAgBlockInfo(clinet: *mut S7Object, blockType: c_int, blockNum: c_int, TS7BlockInfo *pUsrData) -> c_int;
+    // fn Cli_GetPgBlockInfo(clinet: *mut S7Object, pBlock: *mut c_void, TS7BlockInfo *pUsrData, size: c_int) -> c_int;
+    // fn Cli_ListBlocksOfType(clinet: *mut S7Object, blockType: c_int, TS7BlocksOfType *pUsrData, itemsCount: *mut c_int) -> c_int;
+    // Blocks functions
+    // fn Cli_Upload(clinet: *mut S7Object, blockType: c_int, blockNum: c_int, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+    // fn Cli_FullUpload(clinet: *mut S7Object, blockType: c_int, blockNum: c_int, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+    // fn Cli_Download(clinet: *mut S7Object, blockNum: c_int, pUsrData: *mut c_void, size: c_int) -> c_int;
+    // fn Cli_Delete(clinet: *mut S7Object, blockType: c_int, blockNum: c_int) -> c_int;
+    fn Cli_DBGet(
+        clinet: *mut S7Object,
+        dbNumber: c_int,
+        pUsrData: *mut c_void,
+        size: *mut c_int,
+    ) -> c_int;
+    fn Cli_DBFill(clinet: *mut S7Object, dbNumber: c_int, fillChar: c_int) -> c_int;
+    // Date/Time functions
+    // fn Cli_GetPlcDateTime(clinet: *mut S7Object, tm *DateTime) -> c_int;
+    // fn Cli_SetPlcDateTime(clinet: *mut S7Object, tm *DateTime) -> c_int;
+    // fn Cli_SetPlcSystemDateTime(clinet: *mut S7Object) -> c_int;
+    // System Info functions
+    // fn Cli_GetOrderCode(clinet: *mut S7Object, TS7OrderCode *pUsrData) -> c_int;
+    // fn Cli_GetCpuInfo(clinet: *mut S7Object, TS7CpuInfo *pUsrData) -> c_int;
+    // fn Cli_GetCpInfo(clinet: *mut S7Object, TS7CpInfo *pUsrData) -> c_int;
+    // fn Cli_ReadSZL(clinet: *mut S7Object, id: c_int, index: c_int, TS7SZL *pUsrData, size: *mut c_int) -> c_int;
+    // fn Cli_ReadSZLList(clinet: *mut S7Object, TS7SZLList *pUsrData, itemsCount: *mut c_int) -> c_int;
+    // Control functions
+    // fn Cli_PlcHotStart(clinet: *mut S7Object) -> c_int;
+    // fn Cli_PlcColdStart(clinet: *mut S7Object) -> c_int;
+    // fn Cli_PlcStop(clinet: *mut S7Object) -> c_int;
+    // fn Cli_CopyRamToRom(clinet: *mut S7Object, timeout: c_int) -> c_int;
+    // fn Cli_Compress(clinet: *mut S7Object, timeout: c_int) -> c_int;
+    // fn Cli_GetPlcStatus(clinet: *mut S7Object, status: *mut c_int) -> c_int;
+    // Security functions
+    // fn Cli_GetProtection(clinet: *mut S7Object, TS7Protection *pUsrData) -> c_int;
+    // fn Cli_SetSessionPassword(clinet: *mut S7Object, char *Password) -> c_int;
+    // fn Cli_ClearSessionPassword(clinet: *mut S7Object) -> c_int;
+    // Low level
+    // fn Cli_IsoExchangeBuffer(clinet: *mut S7Object, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+    // Misc
+    // fn Cli_GetExecTime(clinet: *mut S7Object, time: *mut c_int) -> c_int;
+    // fn Cli_GetLastError(clinet: *mut S7Object, lastError: *mut c_int) -> c_int;
+    // fn Cli_GetPduLength(clinet: *mut S7Object, requested: *mut c_int, negotiated: *mut c_int) -> c_int;
+    // fn Cli_ErrorText(error: c_int, char *Text, textLen: c_int) -> c_int;
+    // 1.1.0
+    fn Cli_GetConnected(clinet: *mut S7Object, connected: *mut c_int) -> c_int;
 //------------------------------------------------------------------------------
 //  Async functions
 //------------------------------------------------------------------------------
-int S7API Cli_AsReadArea(S7Object Client, int Area, int DBNumber, int Start, int Amount, int WordLen, void *pUsrData);
-int S7API Cli_AsWriteArea(S7Object Client, int Area, int DBNumber, int Start, int Amount, int WordLen, void *pUsrData);
-int S7API Cli_AsDBRead(S7Object Client, int DBNumber, int Start, int Size, void *pUsrData);
-int S7API Cli_AsDBWrite(S7Object Client, int DBNumber, int Start, int Size, void *pUsrData);
-int S7API Cli_AsMBRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsMBWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsEBRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsEBWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsABRead(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsABWrite(S7Object Client, int Start, int Size, void *pUsrData);
-int S7API Cli_AsTMRead(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_AsTMWrite(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_AsCTRead(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_AsCTWrite(S7Object Client, int Start, int Amount, void *pUsrData);
-int S7API Cli_AsListBlocksOfType(S7Object Client, int BlockType, TS7BlocksOfType *pUsrData, int *ItemsCount);
-int S7API Cli_AsReadSZL(S7Object Client, int ID, int Index, TS7SZL *pUsrData, int *Size);
-int S7API Cli_AsReadSZLList(S7Object Client, TS7SZLList *pUsrData, int *ItemsCount);
-int S7API Cli_AsUpload(S7Object Client, int BlockType, int BlockNum, void *pUsrData, int *Size);
-int S7API Cli_AsFullUpload(S7Object Client, int BlockType, int BlockNum, void *pUsrData, int *Size);
-int S7API Cli_AsDownload(S7Object Client, int BlockNum, void *pUsrData, int Size);
-int S7API Cli_AsCopyRamToRom(S7Object Client, int Timeout);
-int S7API Cli_AsCompress(S7Object Client, int Timeout);
-int S7API Cli_AsDBGet(S7Object Client, int DBNumber, void *pUsrData, int *Size);
-int S7API Cli_AsDBFill(S7Object Client, int DBNumber, int FillChar);
-int S7API Cli_CheckAsCompletion(S7Object Client, int *opResult);
-int S7API Cli_WaitAsCompletion(S7Object Client, int Timeout);
+// fn Cli_AsReadArea(clinet: *mut S7Object, area: c_int, dbNumber: c_int, start: c_int, amount: c_int, wordLen: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsWriteArea(clinet: *mut S7Object, area: c_int, dbNumber: c_int, start: c_int, amount: c_int, wordLen: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsDBRead(clinet: *mut S7Object, dbNumber: c_int, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsDBWrite(clinet: *mut S7Object, dbNumber: c_int, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsMBRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsMBWrite(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsEBRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsEBWrite(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsABRead(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsABWrite(clinet: *mut S7Object, start: c_int, size: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsTMRead(clinet: *mut S7Object, start: c_int, amount: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsTMWrite(clinet: *mut S7Object, start: c_int, amount: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsCTRead(clinet: *mut S7Object, start: c_int, amount: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsCTWrite(clinet: *mut S7Object, start: c_int, amount: c_int, pUsrData: *mut c_void) -> c_int;
+// fn Cli_AsListBlocksOfType(clinet: *mut S7Object, blockType: c_int, TS7BlocksOfType *pUsrData, itemsCount: *mut c_int) -> c_int;
+// fn Cli_AsReadSZL(clinet: *mut S7Object, id: c_int, index: c_int, TS7SZL *pUsrData, size: *mut c_int) -> c_int;
+// fn Cli_AsReadSZLList(clinet: *mut S7Object, TS7SZLList *pUsrData, itemsCount: *mut c_int) -> c_int;
+// fn Cli_AsUpload(clinet: *mut S7Object, blockType: c_int, blockNum: c_int, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+// fn Cli_AsFullUpload(clinet: *mut S7Object, blockType: c_int, blockNum: c_int, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+// fn Cli_AsDownload(clinet: *mut S7Object, blockNum: c_int, pUsrData: *mut c_void, size: c_int) -> c_int;
+// fn Cli_AsCopyRamToRom(clinet: *mut S7Object, timeout: c_int) -> c_int;
+// fn Cli_AsCompress(clinet: *mut S7Object, timeout: c_int) -> c_int;
+// fn Cli_AsDBGet(clinet: *mut S7Object, dbNumber: c_int, pUsrData: *mut c_void, size: *mut c_int) -> c_int;
+// fn Cli_AsDBFill(clinet: *mut S7Object, dbNumber: c_int, fillChar: c_int) -> c_int;
+// fn Cli_CheckAsCompletion(clinet: *mut S7Object, opResult: *mut c_int) -> c_int;
+// fn Cli_WaitAsCompletion(clinet: *mut S7Object, timeout: c_int) -> c_int;
 }
 
 fn main() {
